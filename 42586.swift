a3bb8a4799da34dda68cd0e23bec638a85e15996
고재경 <folw159@gmail.com>
2021-11-29 10:24:55 +0900

    add main file

---
+++ 42586.swift
@@ -0,0 +1,9 @@
+//
+//  42586.swift
+//  ForBackJoon
+//
+//  Created by Jae Kyeong Ko on 2021/11/29.
+//  기능개발
+//  https://programmers.co.kr/learn/courses/30/lessons/42586
+
+import Foundation
---
+++ main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  ForBackJoon
+//
+//  Created by Jae Kyeong Ko on 2021/11/29.
+//
+
+import Foundation
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    var answer = [Int]()
+    for i in progresses.indices {
+        progresses[i]
+    }
+    
+    return answer
+}
+
+solution([93, 30, 55], [1, 30, 5])
