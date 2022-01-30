6263c22028d467c54a687861cfe374820ab3c9db
고재경 <folw159@gmail.com>
2022-01-30 18:57:18 +0900

    12918

--- main.swift
+++ main.swift
@@ -5,9 +5,3 @@
 //  Created by Jae Kyeong Ko on 2021/11/29.
 //  
 
-import Foundation
-
-func isHarshad(_ x:Int) -> Bool {
-    let sum = String(x).map { Int(String($0))! }.reduce(0, +)
-    return x % sum == 0 ? true : false
-}
